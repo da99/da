0192c061d3bd1cc84176e9330a6319e56ae0594c
da99 <i-hate-spam-1234567@mailinator.com>
2017-10-14 18:20:24 -0500

    Added: a id spec.

--- specs/specs/03.a.cr
+++ specs/specs/03.a.cr
@@ -27,4 +27,11 @@ describe "a tag" do
     }
     should_eq actual, %(<a href="#invalid">my page</a>)
   end # === it "sanitizes javascript :href values"
+
+  it "allows :id attribute" do
+    actual = A_Spec_HTML.new.render {
+      a("#main", href: "/page") { "the page" }
+    }
+    should_eq actual, %(<a id="main" href="/page">the page</a>)
+  end # === it "allows :id attribute"
 end # === desc "a tag"
--- src/da_html/tags/a.cr
+++ src/da_html/tags/a.cr
@@ -7,7 +7,7 @@ module DA_HTML
       io.write_tag("a") {
 
         {% unless args.empty? %}
-          io.write_id_class {{*args}}
+          io.write_attr_id_class {{*args}}
         {% end %}
 
         {% for k, v in attrs %}
