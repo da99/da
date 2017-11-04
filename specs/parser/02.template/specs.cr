7ac8412384595221ea30e35fa79008e1b81bb810
da99 <i-hate-spam-1234567@mailinator.com>
2017-11-04 09:57:11 -0500

    Touchups.

--- specs/parser/02.template/specs.cr
+++ specs/parser/02.template/specs.cr
@@ -7,40 +7,32 @@ struct SPECS_TEMPLATE
   def_tags :html , :head , :title , :body , :p, :div, :link
   finish_def_html!
 
+  macro spec(name)
+    x = __DIR__ + "{{name.id}}"
+    expect = File.join(x, "expect.html")
+    input  = File.join(x, "input.html")
+    actual = SPECS_TEMPLATE.new("input.html", x).to_html
+    should_eq strip(actual), strip(File.read(expect))
+  end # === macro spec
+
 end # === class SPECS_TEMPLATE
 
 describe "Parser template tag" do
 
   it "adds type attr to script tag" do
-    x = __DIR__ + "/adds_type_attr_to_script_template"
-    expect = File.join(x, "expect.html")
-    input  = File.join(x, "input.html")
-    actual = SPECS_TEMPLATE.new("input.html", x).to_html
-    should_eq strip(actual), strip(File.read(expect))
+    SPECS_TEMPLATE.spec "/adds_type_attr_to_script_template"
   end
 
   it "multi-escapes nested templates" do
-    x = __DIR__ + "/nested_template_tags"
-    expect = File.join(x, "expect.html")
-    input  = File.join(x, "input.html")
-    actual = SPECS_TEMPLATE.new("input.html", x).to_html
-    should_eq strip(actual), strip(File.read(expect))
+    SPECS_TEMPLATE.spec "/nested_template_tags"
   end # === it "multi-escapes nested templates"
 
   it "renders custom attributes" do
-    x = __DIR__ + "/uses_custom_template_attrs"
-    expect = File.join(x, "expect.html")
-    input  = File.join(x, "input.html")
-    actual = SPECS_TEMPLATE.new("input.html", x).to_html
-    should_eq strip(actual), strip(File.read(expect))
+    SPECS_TEMPLATE.spec "/uses_custom_template_attrs"
   end # === it "renders custom attributes"
 
   it "does not override template attrs" do
-    x = __DIR__ + "/no_override_template_attrs"
-    expect = File.join(x, "expect.html")
-    input  = File.join(x, "input.html")
-    actual = SPECS_TEMPLATE.new("input.html", x).to_html
-    should_eq strip(actual), strip(File.read(expect))
+    SPECS_TEMPLATE.spec "/no_override_template_attrs"
   end # === it "does not override template attrs"
 
 end # === desc "parser"
