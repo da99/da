1dd187f3de606ed7d973da66f0ab05b1d6bf721e
da99 <da@bdrm>
2018-06-09 14:37:22 -0500

    Added: is_development?

--- src/da/File_System.cr
+++ src/da/File_System.cr
@@ -1,5 +1,10 @@
 
 module DA
+
+  def is_development?
+    ENV["IS_DEVELOPMENT"]? || ENV["IS_DEV"]?
+  end # === def is_development?
+
   def public_dir_permissions(public_dir : String)
     temp = public_dir
     while temp != "/"
@@ -8,4 +13,5 @@ module DA
     end
     DA.system!("chmod o+rX -R #{public_dir}")
   end # === def public_dir_permissions
+
 end # === module DA
