274fcf85eefc738045245d680c2f52c83387e1f6
da99 <da@bdrm>
2018-06-16 04:20:41 -0500

    Development checkpoint.

--- src/da/VoidLinux.cr
+++ src/da/VoidLinux.cr
@@ -3,18 +3,19 @@ module DA
   module VoidLinux
     extend self
 
-    def install(bin_and_package : String)
-      DA.system!("sudo xbps-install -S #{bin_and_package}")
+    def install(pkg_name : String)
+      install [pkg_name]
     end # === def install
 
-    def install(bin_name : String, package : String)
+    def install(bin_name : String, pkg_name : String)
       `which #{bin_name}`
       if !DA.success?($?)
-        install package
+        install [pkg_name]
       end
     end # === def install
 
     def install(pkgs : Array(String))
+      DA.orange! "{{sudo}} xbps-install -S BOLD{{#{pkgs.join ' '}}}"
       system "sudo xbps-install -S #{pkgs.join ' '}"
       status = $?
       if [6, 17].includes?(status.exit_code)
