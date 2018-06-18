15aebf8463f56beb73a70f5afdfb944c286e64a8
da99 <da@bdrm>
2018-06-18 15:07:38 -0500

    Added: specs for DA.exit!

--- specs/Process.cr
+++ specs/Process.cr
@@ -2,7 +2,30 @@
 describe "Process.exit!" do
   it "raises DA:Exit with Int32, String" do
     assert_raises(DA::Exit) {
-      DA.exit! 3, "My mesage"
+      DA.exit! 3, "My message"
     }
   end # === it "raises DA:Exit with Int32, String"
+
+  it "sets exit_code" do
+    err = assert_raises(DA::Exit) {
+      DA.exit! 3, "My Message"
+    }
+    if err.is_a?(DA::Exit)
+      assert err.exit_code == 3
+    else
+      assert err.class == err.class
+    end
+  end # === it "sets exit_code"
+
+  it "sets .message" do
+    msg = Time.now.to_s
+    err = assert_raises(DA::Exit) {
+      DA.exit! 3, msg
+    }
+    if err.is_a?(DA::Exit)
+      assert err.message == msg
+    else
+      assert err.class == err.class
+    end
+  end # === it "sets exit_code"
 end # === desc "Process.exit!"
