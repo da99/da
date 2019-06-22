463acf695a9a95d3d1d38e8d5cd521a736910cf9
da99 <da@bdrm>
2019-06-22 01:13:17 -0500

    Development checkpoint.

--- specs/Release.cr
+++ specs/Release.cr
@@ -2,9 +2,9 @@
 describe "Release.list" do
   it "returns an Array(String)" do
     dirs = [
-      "app1/#{Time.now.epoch}-ced0de5",
-      "app1/#{Time.now.epoch - 5}-bed0de5",
-      "app1/#{Time.now.epoch - 10}-aed0de5"
+      "app1/#{Time.now.to_unix}-ced0de5",
+      "app1/#{Time.now.to_unix - 5}-bed0de5",
+      "app1/#{Time.now.to_unix - 10}-aed0de5"
     ]
     reset_file_system {
       dirs.each { |d| Dir.mkdir_p d }
