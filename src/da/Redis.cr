52b17e6a86a50ffa4074f24f965cbc68e85b4288
da99 <sp.03+github.spam@miniuni.com>
2019-09-08 20:45:44 -0500

    Started: Helper for Redis pools.

--- src/da/Redis.cr
+++ src/da/Redis.cr
@@ -1,6 +1,25 @@
 
 
-require "da_redis"
-if ENV["DEV_REDIS_PORT"]?
-  DA_Redis.port ENV["DEV_REDIS_PORT"].to_i32
-end
+module DA
+
+  POOL_CLIENTS = Array(Redis::PooledClient).new
+
+  def self.redis_socket_path(conf_path : String)
+    File.read(conf_path).lines.reverse.each { |l|
+      next unless l[/^unixsocket /]?
+        return l.split.last
+    }
+    raise "Redis socket not found!"
+  end # def
+
+  def self.new_redis(conf_path)
+      socket = redis_socket_path(conf_path)
+      DA.inspect! "=== Connecting to: #{socket}"
+      rp = Redis::PooledClient.new(unixsocket: socket)
+      POOL_CLIENTS << rp
+      rp
+  end # def
+
+end # === module
+
+at_exit { DA::POOL_CLIENTS.each { |x| x.close } }
