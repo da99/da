8d76c3a4f26e9c441d8e5c76310db3b8921ed03f
da99 <sp.03+github.spam@miniuni.com>
2020-06-16 15:07:36 -0500

    Added: each_until

--- src/da/Enumerable.cr
+++ src/da/Enumerable.cr
@@ -13,4 +13,17 @@ module DA
       }
     end
   end # def
+
+  def each_until(e, target0
+    index = e.index(target)
+    if index == nil
+      e.each { |x| yield x }
+    else
+      index = index || -1
+      e.each_with_index { |x, i|
+        next if i >= index
+        yield x
+      }
+    end
+  end # def
 end # === module
