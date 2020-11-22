23664edf469d13317ff47dd4a0cb43ced77b5b7c
da99 <sp.03+github.spam@miniuni.com>
2020-11-21 23:24:14 -0600

    Added: spec to ensure at least one assertion is specified.

---
+++ spec/specs/02.assertion_required.cr
@@ -0,0 +1,15 @@
+
+if !in_spec?
+  describe("it") {
+    it("fails if no assertion specified.") {
+    }
+  }
+else
+  describe("it") {
+    it("fails if no assertion specified.") {
+      result = run("it fails if no assertion specified.")
+      actual = result.exit_code
+      assert actual != 0
+    }
+  }
+end # if
