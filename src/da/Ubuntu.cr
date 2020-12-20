a127c42cc1d85007f156cb3f55c48b432abab8f1
da99 <sp.03+github.spam@miniuni.com>
2020-12-20 15:49:23 -0600

    Development checkpoint.

--- src/da/Ubuntu.cr
+++ src/da/Ubuntu.cr
@@ -4,7 +4,9 @@ module DA
     extend self
 
     def upgrade
-      DA::Process::Inherit.new("sudo journalctl --vacuum-time=1d").success!
+      DA::Process::Inherit.new("sudo apt-get autoclean").success!
+      DA::Process::Inherit.new("sudo apt-get clean").success!
+      # DA::Process::Inherit.new("sudo journalctl --vacuum-size=500M").success!
       DA::Process::Inherit.new("sudo apt-get update").success!
       DA::Process::Inherit.new("sudo apt-get upgrade").success!
       DA::Process::Inherit.new("sudo apt autoremove").success!
