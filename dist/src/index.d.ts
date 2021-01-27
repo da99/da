ef99f8c338897bdb6ec935bac2044034a3b3ca8f
da99 <sp.03+github.spam@miniuni.com>
2021-01-27 08:30:50 -0600

    Removed: chalk dependency.

--- dist/src/index.d.ts
+++ dist/src/index.d.ts
@@ -1,5 +1,4 @@
-import chalk from "chalk";
 export declare function describe(name: any, f: any): void;
 export declare function it(name: any, f: any): void;
 import { strict as assert } from "assert";
-export { assert, chalk };
+export { assert };
