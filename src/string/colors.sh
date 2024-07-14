dc96fc20ebe774c4a1b75b02eb6509fc9da82667
da99 <1551848+da99@users.noreply.github.com>
2024-07-14 08:16:36 -0500

    Development checkpoint.

---
+++ src/string/colors.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+#
+#
+set -u -e -o pipefail
+
+
+cBOLD=$(tput bold)
+sNORMAL=$(tput sgr0)
+sRESET="$sNORMAL"
+sITALIC="\e[3m"
+sRED="\e[31m"
+sGREEN="\e[32m"
+sYELLOW="\e[33m"
+sBLUE="\e[34m"
+sWHILE="\e[37m"
+
