2b2a257362e26fd6eda992614b75107d1c97c966
da99 <spam.i.hate@miniuni.com>
2024-08-30 12:49:19 -0500

    Development checkpoint.

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -392,6 +392,7 @@ require('mini.pairs').setup()
 require('mini.comment').setup()
 require('mini.bracketed').setup()
 require('mini.surround').setup({})
+require('mini.align').setup()
 
 require('mini.completion').setup({
   mappings = {
