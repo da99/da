9f6639f3cca5292cdb6d5ea9b31ba42844600696
da99 <spam.i.hate@miniuni.com>
2024-10-29 09:47:04 -0500

    Development checkpoint.

--- src/windows/windows.rb
+++ src/windows/windows.rb
@@ -24,21 +24,28 @@ class Root_Window
   end
 
   def top_padding
-    50
+    40
   end
 
   def bottom_padding
     50
   end
 
-  def gap
-    10
-  end
 end # class
 
 ROOT = Root_Window.new
 
 class Window
+  class << self
+    def border
+      4
+    end
+
+    def margin
+      10
+    end
+  end # class
+
   attr_reader :id, :x, :y, :w, :h, :border_x, :border_y
 
   def initialize(raw_id = nil)
@@ -78,7 +85,7 @@ class Window
   end
 
   def move_to(pos)
-    system(%[wmctrl -i -r #{id} -e 0,#{pos.x},#{pos.y},#{pos.w},#{pos.h}])
+    system(%( wmctrl -i -r #{id} -e 0,#{pos.x},#{pos.y},#{pos.w},#{pos.h} ))
   end
 
   def mouse_location
@@ -100,7 +107,7 @@ module Left_Side
   end
 
   def w
-    (ROOT.w * 0.70).to_i - ROOT.left_padding - ROOT.gap
+    (ROOT.w * 0.70).to_i - ROOT.left_padding - Window.margin
   end
 
   def h
@@ -113,7 +120,7 @@ module Right_Top
   extend self
 
   def x
-    (ROOT.w * 0.30).to_i + ROOT.gap
+    (ROOT.w * 0.70).to_i + Window.margin
   end
 
   def y
@@ -121,11 +128,73 @@ module Right_Top
   end
 
   def w
-    ROOT.w - x - ROOT.right_padding
+    ROOT.w - x - Window.border
   end
 
   def h
-    (ROOT.h/2).to_i - ROOT.top_padding - ROOT.gap
+    (ROOT.h / 2).to_i - ROOT.top_padding - Window.margin
+  end
+end # module
+
+module Right_Bottom
+  extend self
+
+  def x
+    (ROOT.w * 0.70).to_i + Window.margin
+  end
+
+  def y
+    (ROOT.h / 2).to_i + Window.margin
+  end
+
+  def w
+    ROOT.w - x - Window.border
+  end
+
+  def h
+    (ROOT.h / 2).to_i - Window.margin - Window.border
+  end
+end # module
+
+module Bottom_Stamp
+  extend self
+
+  def x
+    ROOT.w - w - Window.border
+  end
+
+  def y
+    ROOT.h - h - Window.border
+  end
+
+  def w
+    return (1920 * 0.15).to_i if ROOT.h == 1080
+
+    (ROOT.w * 0.15).to_i
+  end
+
+  def h
+    (ROOT.h * 0.15).to_i
+  end
+end # module
+
+module Fullscreen
+  extend self
+
+  def x
+    ROOT.left_padding
+  end
+
+  def y
+    ROOT.top_padding
+  end
+
+  def w
+    ROOT.w - x - Window.border - Window.margin
+  end
+
+  def h
+    ROOT.h - ROOT.top_padding - ROOT.bottom_padding
   end
 end # module
 
@@ -137,13 +206,22 @@ module Pointer_Location
   extend self
 
   def edge_name(window, mouse)
-    tb = top_edge?(window, mouse) ? 'top' : (bottom_edge?(window, mouse) ? 'bottom' : nil)
-    lr = left_edge?(window, mouse) ? 'left' : (right_edge?(window, mouse) ? 'right' : nil)
+    tb = if top_edge?(window, mouse)
+           'top'
+         else
+           (bottom_edge?(window, mouse) ? 'bottom' : nil)
+         end
+    lr = if left_edge?(window, mouse)
+           'left'
+         else
+           (right_edge?(window, mouse) ? 'right' : nil)
+         end
 
     return "#{tb}_#{lr}_corner" if tb && lr
     return "#{tb}_edge" if tb
     return "#{lr}_edge" if lr
-    return "center" if center?(window, mouse)
+
+    'center' if center?(window, mouse)
   end
 
   def top_edge?(window, mouse)
@@ -163,8 +241,8 @@ module Pointer_Location
   end
 
   def center?(window, mouse)
-    center_x = window.x + (window.w/2).to_i
-    center_y = window.y + (window.h/2).to_i
+    center_x = window.x + (window.w / 2).to_i
+    center_y = window.y + (window.h / 2).to_i
     padding = 20
     half_corner = (CORNER_AREA + padding).to_i
     mouse.x >= (center_x - half_corner) && mouse.x < (center_x + half_corner) &&
@@ -174,8 +252,9 @@ end # class
 
 class Mouse_Pointer
   attr_reader :x, :y
+
   def initialize
-    raw_x, raw_y, raw_screen, raw_window = `xdotool getmouselocation --shell`.strip.split("\n")
+    raw_x, raw_y, _raw_screen, _raw_window = `xdotool getmouselocation --shell`.strip.split("\n")
     @x = raw_x.split('=').last.to_i
     @y = raw_y.split('=').last.to_i
   end
@@ -202,6 +281,23 @@ when 'mouse_location'
 when 'move_to left'
   Window.new.move_to(Left_Side)
   exit($?.exitstatus)
+
+when 'move_to bottom_stamp'
+  Window.new.move_to(Bottom_Stamp)
+  exit($?.exitstatus)
+
+when 'move_to right_top'
+  Window.new.move_to(Right_Top)
+  exit($?.exitstatus)
+
+when 'move_to right_bottom'
+  Window.new.move_to(Right_Bottom)
+  exit($?.exitstatus)
+
+when 'move_to fullscreen'
+  Window.new.move_to(Fullscreen)
+  exit($?.exitstatus)
+
 
 else
   warn "!!! Unknown command: #{cmd}"
