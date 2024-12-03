9e300bc79632d06b80964bde14cb6c9f3860c462
da99 <spam.i.hate@miniuni.com>
2024-12-03 16:55:29 -0600

    Development checkpoint.

--- src/windows/State.rb
+++ src/windows/State.rb
@@ -2,19 +2,41 @@
 
 class State
   LOCAL_DATA = '/tmp/window_rb'
+  attr_reader :window
 
-  def initialize; end
+  def initialize(win)
+    @window = win
+  end # def
 
-  def write_position(window, location)
-    file_name = "#{window.id}.location.txt"
-    content = location.name.to_s
-    begin
-      File.write "#{LOCAL_DATA}/#{file_name}", content
-    rescue Errno::ENOENT => _e
-      `mkdir -p "#{LOCAL_DATA}"`
-      File.write "#{LOCAL_DATA}/#{file_name}", content
+  class << self
+    def move_window(window, location)
+      state = State.new(window)
+      case window.wm_class
+      when 'smplayer.smplayer'
+        case location
+        when Bottom_Stamp
+          # Ctrl-C
+        else
+          if state.location?(Bottom_Stamp)
+            # Ctrl-C
+          end
+        end # case
+      else
+        window.move_to(location)
+      end
+    end # def
+
+    def write_position(window, location)
+      file_name = "#{window.id}.location.txt"
+      content = location.name.to_s
+      begin
+        File.write "#{LOCAL_DATA}/#{file_name}", content
+      rescue Errno::ENOENT => _e
+        `mkdir -p "#{LOCAL_DATA}"`
+        File.write "#{LOCAL_DATA}/#{file_name}", content
+      end
     end
-  end
+  end # class
 end # class
 
 STATE = State.new
