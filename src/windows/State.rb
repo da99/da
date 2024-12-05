0cc9deabc42b57c356a3af73bd60a63fd293aea6
da99 <1551848+da99@users.noreply.github.com>
2024-12-04 19:08:26 -0600

    Development checkpoint.

--- src/windows/State.rb
+++ src/windows/State.rb
@@ -1,11 +1,27 @@
 # frozen_string_literal: true
 
+def ctrl_c
+  `xdotool sleep 0.1 key --clearmodifiers ctrl+c`
+end
+
 class State
   LOCAL_DATA = '/tmp/window_rb'
   attr_reader :window
 
   def initialize(win)
     @window = win
+  end # def
+
+  def write_previous_location(location)
+    State.write(window, 'previous_location', location.name.to_s)
+  end # def
+
+  def write_location(location)
+    State.write(window, 'location', location.name)
+  end # def
+
+  def location?(location)
+    State.read(window, 'location') == location.name.to_s
   end # def
 
   class << self
@@ -15,28 +31,34 @@ class State
       when 'smplayer.smplayer'
         case location
         when Bottom_Stamp
-          # Ctrl-C
+          ctrl_c
+          window.move_to(location)
         else
           if state.location?(Bottom_Stamp)
-            # Ctrl-C
+            ctrl_c
           end
         end # case
-      else
-        window.move_to(location)
       end
+
+      window.move_to(location)
     end # def
 
-    def write_position(window, location)
-      file_name = "#{window.id}.location.txt"
-      content = location.name.to_s
+    def read(window, title)
+      File.read("#{LOCAL_DATA}/#{window.id}.#{title}.txt")
+    rescue Errno::ENOENT => _e
+      ''
+    end # def
+
+    def write(window, title, content)
+      file_name = "#{LOCAL_DATA}/#{window.id}.#{title}.txt"
       begin
-        File.write "#{LOCAL_DATA}/#{file_name}", content
+        File.write file_name, content.to_s
       rescue Errno::ENOENT => _e
         `mkdir -p "#{LOCAL_DATA}"`
-        File.write "#{LOCAL_DATA}/#{file_name}", content
+        File.write file_name, content
       end
-    end
-  end # class
+    end # der
+  end # class << self
 end # class
 
 STATE = State.new
