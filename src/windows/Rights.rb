30cab907b3d81e73071ac7e4a57baf28db30a4d1
da99 <1551848+da99@users.noreply.github.com>
2024-12-15 15:49:53 -0600

    Development checkpoint.

--- src/windows/Rights.rb
+++ src/windows/Rights.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-module Right_Top
+module Right_Side
   extend self
 
   def stamp?
@@ -20,7 +20,55 @@ module Right_Top
   end
 
   def h
+    Window.root.h - Window.root.bottom_padding - y
+  end
+end # module
+
+module Right_Top
+  extend self
+
+  def stamp?
+    false
+  end
+
+  def x
+    Right_Side.x
+  end
+
+  def y
+    Right_Side.y
+  end
+
+  def w
+    Right_Side.w
+  end
+
+  def h
     (Window.root.h / 2).to_i - Window.root.top_padding - Window.margin
+  end
+end # module
+
+module Right_Bottom
+  extend self
+
+  def stamp?
+    false
+  end
+
+  def x
+    Right_Side.x
+  end
+
+  def y
+    (Window.root.h / 2).to_i + Window.margin
+  end
+
+  def w
+    Right_Side.w
+  end
+
+  def h
+    (Window.root.h / 2).to_i - Window.margin - Window.border
   end
 end # module
 
@@ -47,52 +95,3 @@ module Right_Half
     Window.root.h - Window.root.bottom_padding - y
   end
 end # module
-
-module Right_Side
-  extend self
-
-  def stamp?
-    false
-  end
-
-  def x
-    (Window.root.w * 0.70).to_i + Window.margin
-  end
-
-  def y
-    Window.root.top_padding
-  end
-
-  def w
-    Window.root.w - x - Window.border
-  end
-
-  def h
-    Window.root.h - Window.root.bottom_padding - y
-  end
-end # module
-
-module Right_Bottom
-  extend self
-
-  def stamp?
-    false
-  end
-
-  def x
-    (Window.root.w * 0.70).to_i + Window.margin
-  end
-
-  def y
-    (Window.root.h / 2).to_i + Window.margin
-  end
-
-  def w
-    Window.root.w - x - Window.border
-  end
-
-  def h
-    (Window.root.h / 2).to_i - Window.margin - Window.border
-  end
-end # module
-
