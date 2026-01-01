a269381d906f36d6e4d89dfb5883226c3516cd6c
da99 <1551848+da99@users.noreply.github.com>
2026-01-01 10:34:36 -0600

    Development checkpoint.

--- templates/script.sh
+++ templates/script.sh
@@ -1,13 +1,15 @@
-#!/usr/bin/env ash
+#!/usr/bin/env sh
 #
 #
 set -u -e -o pipefail
 
+log() { echo "--- $*" >&2; }
 
 case "$*" in
   "-h"|"--help")
     echo "$0 -h|--help -- This message."
     ;;
+
   *)
     echo "!!! Unknown command: $*" >&2
     exit 1
